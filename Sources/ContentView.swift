ee8775dd42f835a927ee3a0765218416723894c4
MargotPasquali <margotpasquali1@gmail.com>
2025-05-06 15:48:23 +0200

    add custom gitignore

---
+++ Sources/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Unsolved
+//
+//  Created by Margot Pasquali on 06/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Sources/UnsolvedApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnsolvedApp.swift
+//  Unsolved
+//
+//  Created by Margot Pasquali on 06/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct UnsolvedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
